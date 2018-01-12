5b544a045401f74fbfbae15ccd6ce0a8a07fc85d
David Anderson <david.anderson@ntlworld.com>
2018-01-12 17:19:29 +0000

    Second commit

--- TestGithub/src/test_package/HelloWorldSWT.java
+++ TestGithub/src/test_package/HelloWorldSWT.java
@@ -6,11 +6,10 @@ import org.eclipse.swt.widgets.*;
 public class HelloWorldSWT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setText("Hello world!");
+		shell.setText("Hello world! Now within github");
 		Label label = new Label(shell, SWT.BORDER);
 		label.setText("Another Hello World - after changing this line !");
 		label.pack();
